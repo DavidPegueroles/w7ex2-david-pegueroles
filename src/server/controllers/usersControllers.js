4029c63056d5d44f37426303aa5b201ca93ebe36
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-22 15:09:50 +0100

    Added usersLogin function

--- src/server/controllers/usersControllers.js
+++ src/server/controllers/usersControllers.js
@@ -1,5 +1,35 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../../database/models/User");
+
+const usersLogin = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      const error = new Error("Username does not exist");
+      error.code = 401;
+      next(error);
+    } else {
+      const rightPassword = bcrypt.compare(password, user.password);
+      if (!rightPassword) {
+        const error = new Error("Password incorrect");
+        error.code = 401;
+        next(error);
+      }
+      const userData = {
+        username: user.username,
+        id: user.id,
+      };
+      const token = jwt.sign(userData, process.env.JWT_TOKEN, {
+        expiresIn: "7d",
+      });
+      res.json({ token });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
 const usersRegister = async (req, res, next) => {
   try {
@@ -12,4 +42,4 @@ const usersRegister = async (req, res, next) => {
   }
 };
 
-module.exports = { usersRegister };
+module.exports = { usersLogin, usersRegister };
