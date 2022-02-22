a67ef9742a0b6b315cd7da6c596c8a174ccde6af
DavidPegueroles <david.peguerolesbel@gmail.com>
2022-02-22 15:10:19 +0100

    Added route to usersLogin controller and installed jasonwebtoken

--- src/server/routers/usersRouter.js
+++ src/server/routers/usersRouter.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { usersRegister } = require("../controllers/usersControllers");
+const {
+  usersRegister,
+  usersLogin,
+} = require("../controllers/usersControllers");
 
 const router = express.Router();
 
+router.post("/login", usersLogin);
 router.post("/register", usersRegister);
 
 module.exports = router;
